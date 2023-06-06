e16ea0cfc2160becc5db66825841d9cd1fade772
mohammad mostajeran <mostajeran.mohammad.1999@gmail.com>
2023-06-06 17:18:59 +0330

    singleton validator

--- pkg/utils/dto-validator.go
+++ pkg/utils/dto-validator.go
@@ -2,7 +2,13 @@ package utils
 
 import "github.com/go-playground/validator/v10"
 
+var validate *validator.Validate
+var initialized bool
+
 func ValidateDto(d interface{}) error {
-	validate := validator.New()
+	if !initialized {
+		validate = validator.New()
+		initialized = true
+	}
 	return validate.Struct(d)
 }
