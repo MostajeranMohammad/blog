bf2524e86417b3ea0b663229fa6c355e10a963b9
mohammad mostajeran <mostajeran.mohammad.1999@gmail.com>
2023-06-20 11:55:04 +0330

    fix config load .env file

--- config/config.go
+++ config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 
+	"github.com/MostajeranMohammad/blog/pkg/utils"
 	"github.com/ilyakaznacheev/cleanenv"
 )
 
@@ -17,24 +18,24 @@ type (
 
 	// App -.
 	App struct {
-		Name    string `env-required:"true" yaml:"name"    env:"APP_NAME"`
-		Version string `env-required:"true" yaml:"version" env:"APP_VERSION"`
+		Name    string `validate:"required"   yaml:"name"    env:"APP_NAME"`
+		Version string `validate:"required"   yaml:"version" env:"APP_VERSION"`
 	}
 
 	// HTTP -.
 	HTTP struct {
-		JwtSecret string `env-required:"true" yaml:"jwt_secret" env:"JWT_SECRET"`
-		Port      string `env-required:"true" yaml:"port" env:"HTTP_PORT"`
+		JwtSecret string `validate:"required" yaml:"jwt_secret" env:"JWT_SECRET"`
+		Port      string `validate:"required" yaml:"port"       env:"HTTP_PORT"`
 	}
 
 	// Log -.
 	Log struct {
-		Level string `env-required:"true" yaml:"log_level"   env:"LOG_LEVEL"`
+		Level string `validate:"required"     yaml:"log_level" env:"LOG_LEVEL"`
 	}
 
 	// PG -.
 	PG struct {
-		DSN string `env-required:"true" yaml:"pg_dsn"   env:"PG_DSN"`
+		DSN string ` validate:"required"      yaml:"pg_dsn"    env:"PG_DSN"`
 	}
 )
 
@@ -42,15 +43,30 @@ type (
 func NewConfig() (*Config, error) {
 	cfg := &Config{}
 
-	err := cleanenv.ReadConfig("./config/config.yml", cfg)
+	err := parseConfigFiles([]string{"./config/config.yml", "./.env"}, cfg)
 	if err != nil {
 		return nil, fmt.Errorf("config error: %w", err)
 	}
 
-	err = cleanenv.ReadEnv(cfg)
+	err = cleanenv.UpdateEnv(cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	err = utils.ValidateDto(cfg)
 	if err != nil {
 		return nil, err
 	}
 
 	return cfg, nil
+}
+
+func parseConfigFiles(files []string, cfg *Config) error {
+	for _, path := range files {
+		err := cleanenv.ReadConfig(path, cfg)
+		if err != nil {
+			return fmt.Errorf("config error: %w", err)
+		}
+	}
+	return nil
 }
